dd2b549ff0c5c5190bee194aa9e74739be0fd59d
Ravi M.P <madscience@hush.com>
2022-08-27 13:36:59 -0300

    create validation function that contains the conditions (horizontal, vertical, diagonal) necessary for one of the players to win; create a quadrantsAll function that contains the logic that will switch from player1 to player2 and vice-versa; add quadrantsAll in each quadrant function/button to take the respective number to pass as a parameter; add validation function to quadrantsAll so as to validate the conditions in the if statements

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,117 +1,94 @@
 console.log('tic-tac-toe');
 
-let player = '';
+let player = 'player1';
 let firstChoice = ['player11'];
-console.log(firstChoice);
 let firstChoice1 = 'player11';
-let count = 0
-console.log(count);
+let captureFirstChoice;
 
 let quadrants = {
     1: '',
     2: '',
     3: '',
-    4: ''
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: ''
+};
+
+function validation(val) {
+    //console.log(val);
+    if((quadrants[1] === 'player1' && quadrants[2] === 'player1' && quadrants[3] === 'player1') || (quadrants[4] === 'player1' && quadrants[5] === 'player1' && quadrants[6] === 'player1') || (quadrants[7] === 'player1' && quadrants[8] === 'player1' && quadrants[9] === 'player1') //horizontal
+    || (quadrants[1] === 'player1' && quadrants[4] === 'player1' && quadrants[7] === 'player1') || (quadrants[2] === 'player1' && quadrants[5] === 'player1' && quadrants[8] === 'player1') || (quadrants[3] === 'player1' && quadrants[6] === 'player1' && quadrants[9] === 'player1') //vertical
+    || (quadrants[1] === 'player1' && quadrants[5] === 'player1' && quadrants[9] === 'player1') || (quadrants[3] === 'player1' && quadrants[5] === 'player1' && quadrants[7] === 'player1')) { //diagonal
+        console.log('player 1 wins');
+    };
+    if((quadrants[1] === 'player2' && quadrants[2] === 'player2' && quadrants[3] === 'player2') || (quadrants[4] === 'player2' && quadrants[5] === 'player2' && quadrants[6] === 'player2') || (quadrants[7] === 'player2' && quadrants[8] === 'player2' && quadrants[9] === 'player2') //horizontal
+    || (quadrants[1] === 'player2' && quadrants[4] === 'player2' && quadrants[7] === 'player2') || (quadrants[2] === 'player2' && quadrants[5] === 'player2' && quadrants[8] === 'player2') || (quadrants[3] === 'player2' && quadrants[6] === 'player2' && quadrants[9] === 'player2') //vertical
+    || (quadrants[1] === 'player2' && quadrants[5] === 'player2' && quadrants[9] === 'player2') || (quadrants[3] === 'player2' && quadrants[5] === 'player2' && quadrants[7] === 'player2')) {
+        console.log('player 2 wins');
+    };
 };
 console.log(quadrants);
 
 let lever = 'off';
-function quadrantOne() {
-    let captureFirstChoice;
-    captureFirstChoice = firstChoice.pop();
-
-    console.log(firstChoice);
+function quadrantsAll(val) {
     if(firstChoice1 === captureFirstChoice) {
-        quadrants[1] = 'player1';
+        quadrants[val] = player;
         lever = 'on'; 
     };    
-        if(quadrants[1] !== 'player1') {
+        if(quadrants[val] !== 'player1') {
             player = 'player2';
             if(player === 'player2' && lever === 'on') {
-                quadrants[1] = 'player2';
+                quadrants[val] = 'player2';
                 lever = 'off'
             }
-            if(lever === 'off' && quadrants[1] !== 'player2') {
+            if(lever === 'off' && quadrants[val] !== 'player2') {
                 player = 'player1';
                 if(player === 'player1') {
-                    quadrants[1] = 'player1';
-                    lever = 'on';
-                };
-            };
-
+                    quadrants[val] = 'player1';
+                    lever = 'on';                    
+                };  
+            };  
         };
+        validation();
+};
+function quadrantOne() {
+    captureFirstChoice = firstChoice.pop();
+    quadrantsAll(1);
 };
 function quadrantTwo() {
-    let captureFirstChoice;
     captureFirstChoice = firstChoice.pop();
-    console.log(firstChoice);
-    if(firstChoice1 === captureFirstChoice) {
-        quadrants[2] = 'player1';
-        lever = 'on'; 
-    };    
-        if(quadrants[2] !== 'player1') {
-            
-            player = 'player2';
-            if(player === 'player2' && lever === 'on') {
-                quadrants[2] = 'player2';
-                lever = 'off'
-            }
-            if(lever === 'off' && quadrants[2] !== 'player2' ) {
-                player = 'player1';
-                if(player === 'player1') {
-                    quadrants[2] = 'player1';
-                    lever = 'on';
-                };
-            };
-
-        }; 
+    quadrantsAll(2);
 };
 function quadrantThree() {
     captureFirstChoice = firstChoice.pop();
-    console.log(firstChoice);
-    if(firstChoice1 === captureFirstChoice) {
-        quadrants[3] = 'player1'; 
-        lever = 'on';
-    };    
-        if(quadrants[3] !== 'player1') {
-            player = 'player2';
-            
-            if(player === 'player2' && lever === 'on') {
-                quadrants[3] = 'player2';
-                lever = 'off'
-            }
-            if(lever === 'off' && quadrants[3] !== 'player2') {
-                player = 'player1';
-                if(player === 'player1') {
-                    quadrants[3] = 'player1';
-                    lever = 'on';
-                };
-            };
-        }; 
+    quadrantsAll(3);
 };
 function quadrantFour() {
     captureFirstChoice = firstChoice.pop();
-    console.log(firstChoice);
-    if(firstChoice1 === captureFirstChoice) {
-        quadrants[4] = 'player1'; 
-        lever = 'on';
-    };    
-        if(quadrants[4] !== 'player1') {
-            player = 'player2';
-            
-            if(player === 'player2' && lever === 'on') {
-                quadrants[4] = 'player2';
-                lever = 'off'
-            }
-            if(lever === 'off' && quadrants[4] !== 'player2') {
-                player = 'player1';
-                if(player === 'player1') {
-                    quadrants[4] = 'player1';
-                    lever = 'on';
-                };
-            };
-
-        }; 
+    quadrantsAll(4);
+};
+function quadrantFive() {
+    captureFirstChoice = firstChoice.pop();
+    quadrantsAll(5);
+};
+function quadrantSix() {
+    captureFirstChoice = firstChoice.pop();
+    quadrantsAll(6);
+};
+function quadrantSeven() {
+    captureFirstChoice = firstChoice.pop();
+    quadrantsAll(7);
+};
+function quadrantEight() {
+    captureFirstChoice = firstChoice.pop();
+    quadrantsAll(8);
+};
+function quadrantNine() {
+    captureFirstChoice = firstChoice.pop();
+    quadrantsAll(9);
 };
 
 
