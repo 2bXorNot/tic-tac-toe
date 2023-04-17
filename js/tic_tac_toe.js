1b007ba2d3fa0843c206d0fd5662bedce32415ea
2bXorNot <64798261+Ravi-Maia@users.noreply.github.com>
2023-04-17 14:59:31 -0300

    Add CSS file; rewrite quadrant as grid in JS.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -5,7 +5,7 @@ let firstChoice = ['player11'];
 let firstChoice1 = 'player11';
 let captureFirstChoice;
 
-let quadrants = {
+let grid = {
     1: '',
     2: '',
     3: '',
@@ -19,78 +19,93 @@ let quadrants = {
 
 function validation(val) {
     //console.log(val);
-    if((quadrants[1] === 'player1' && quadrants[2] === 'player1' && quadrants[3] === 'player1') || (quadrants[4] === 'player1' && quadrants[5] === 'player1' && quadrants[6] === 'player1') || (quadrants[7] === 'player1' && quadrants[8] === 'player1' && quadrants[9] === 'player1') //horizontal
-    || (quadrants[1] === 'player1' && quadrants[4] === 'player1' && quadrants[7] === 'player1') || (quadrants[2] === 'player1' && quadrants[5] === 'player1' && quadrants[8] === 'player1') || (quadrants[3] === 'player1' && quadrants[6] === 'player1' && quadrants[9] === 'player1') //vertical
-    || (quadrants[1] === 'player1' && quadrants[5] === 'player1' && quadrants[9] === 'player1') || (quadrants[3] === 'player1' && quadrants[5] === 'player1' && quadrants[7] === 'player1')) { //diagonal
+    if((grid[1] === 'player1' && grid[2] === 'player1' && grid[3] === 'player1') || (grid[4] === 'player1' && grid[5] === 'player1' && grid[6] === 'player1') || (grid[7] === 'player1' && grid[8] === 'player1' && grid[9] === 'player1') //horizontal
+    || (grid[1] === 'player1' && grid[4] === 'player1' && grid[7] === 'player1') || (grid[2] === 'player1' && grid[5] === 'player1' && grid[8] === 'player1') || (grid[3] === 'player1' && grid[6] === 'player1' && grid[9] === 'player1') //vertical
+    || (grid[1] === 'player1' && grid[5] === 'player1' && grid[9] === 'player1') || (grid[3] === 'player1' && grid[5] === 'player1' && grid[7] === 'player1')) { //diagonal
         console.log('player 1 wins');
     };
-    if((quadrants[1] === 'player2' && quadrants[2] === 'player2' && quadrants[3] === 'player2') || (quadrants[4] === 'player2' && quadrants[5] === 'player2' && quadrants[6] === 'player2') || (quadrants[7] === 'player2' && quadrants[8] === 'player2' && quadrants[9] === 'player2') //horizontal
-    || (quadrants[1] === 'player2' && quadrants[4] === 'player2' && quadrants[7] === 'player2') || (quadrants[2] === 'player2' && quadrants[5] === 'player2' && quadrants[8] === 'player2') || (quadrants[3] === 'player2' && quadrants[6] === 'player2' && quadrants[9] === 'player2') //vertical
-    || (quadrants[1] === 'player2' && quadrants[5] === 'player2' && quadrants[9] === 'player2') || (quadrants[3] === 'player2' && quadrants[5] === 'player2' && quadrants[7] === 'player2')) {
+    if((grid[1] === 'player2' && grid[2] === 'player2' && grid[3] === 'player2') || (grid[4] === 'player2' && grid[5] === 'player2' && grid[6] === 'player2') || (grid[7] === 'player2' && grid[8] === 'player2' && grid[9] === 'player2') //horizontal
+    || (grid[1] === 'player2' && grid[4] === 'player2' && grid[7] === 'player2') || (grid[2] === 'player2' && grid[5] === 'player2' && grid[8] === 'player2') || (grid[3] === 'player2' && grid[6] === 'player2' && grid[9] === 'player2') //vertical
+    || (grid[1] === 'player2' && grid[5] === 'player2' && grid[9] === 'player2') || (grid[3] === 'player2' && grid[5] === 'player2' && grid[7] === 'player2')) {
         console.log('player 2 wins');
     };
 };
-console.log(quadrants);
+console.log(grid);
 
 let lever = 'off';
-function quadrantsAll(val) {
+function gridAll(val, val2) {
+    let q1 = 'Q1'
     if(firstChoice1 === captureFirstChoice) {
-        quadrants[val] = player;
-        lever = 'on'; 
+        grid[val] = player;
+        lever = 'on';
+        console.log(`${player} chose ${val2}`); 
     };    
-        if(quadrants[val] !== 'player1') {
+        if(grid[val] !== 'player1') {
             player = 'player2';
             if(player === 'player2' && lever === 'on') {
-                quadrants[val] = 'player2';
+                grid[val] = 'player2';
+                console.log(`${player} chose ${val2}`); 
                 lever = 'off'
             }
-            if(lever === 'off' && quadrants[val] !== 'player2') {
+            if(lever === 'off' && grid[val] !== 'player2') {
                 player = 'player1';
                 if(player === 'player1') {
-                    quadrants[val] = 'player1';
+                    grid[val] = 'player1';
+                    console.log(`${player} chose ${val2}`); 
                     lever = 'on';                    
                 };  
             };  
         };
         validation();
+        //changeBg(player);
 };
+
+/*
+function changeBg(button){
+    if(button.id == "q1button" && button === 'player1'){
+        button.style.background = "green";
+        //document.getElementById("q2button").style.background = "transparent";
+        //document.getElementById("q3button").style.background = "transparent";
+    };
+};*/
+
 function quadrantOne() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(1);
+    gridAll(1, 'Q1');
+  
 };
 function quadrantTwo() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(2);
+    gridAll(2, 'Q2');
 };
 function quadrantThree() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(3);
+    gridAll(3, 'Q3');
 };
 function quadrantFour() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(4);
+    gridAll(4, 'Q4');
 };
 function quadrantFive() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(5);
+    gridAll(5, 'Q5');
 };
 function quadrantSix() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(6);
+    gridAll(6, 'Q6');
 };
 function quadrantSeven() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(7);
+    gridAll(7, 'Q7');
 };
 function quadrantEight() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(8);
+    gridAll(8, 'Q8');
 };
 function quadrantNine() {
     captureFirstChoice = firstChoice.pop();
-    quadrantsAll(9);
+    gridAll(9, 'Q9');
 };
-
 
 
 
